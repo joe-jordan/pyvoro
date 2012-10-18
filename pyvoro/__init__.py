8f8bbb0c240cf160e29c09301920151a56f6a5b3
Joe Jordan <joe.jordan@imperial.ac.uk>
2012-10-18 11:32:55 +0100

    updating init py wrapper to ensure docstring is preserved when importing the lazy way.

--- pyvoro/__init__.py
+++ pyvoro/__init__.py
@@ -1,1 +1,33 @@
-from voroplusplus import compute_voronoi
+import voroplusplus
+
+def compute_voronoi(points, limits, dispersion):
+  """
+Input arg formats:
+  points = list of 3-vectors (lists or compatible class instances) of doubles,
+    being the coordinates of the points to voronoi-tesselate.
+  limits = 3-list of 2-lists, specifying the start and end sizes of the box the
+    points are in.
+  dispersion = max distance between two points that might be adjacent (sets
+    voro++ block sizes.)
+  
+Output format is a list of cells as follows:
+  [ # list in same order as original points.
+    {
+      'volume' : 1.0,
+      'vetices' : [[1.0, 2.0, 3.0], ...], # positions of vertices
+      'adjacency' : [[1,3,4, ...], ...], # cell-vertices adjacent to i by index
+      'faces' : [
+        {
+          'vertices' : [7,4,13, ...], # vertex ids in loop order
+          'adjacent_cell' : 34 # *cell* id
+        }, ...]
+      'original' : point[index] # the original instance from args
+    },
+    ... 
+  ]
+  
+  NOTE: The class from items in input points list is reused for all 3-vector
+  outputs. It must have a contructor which accepts a list of 3 python floats
+  (python's list type does satisfy this requirement.)
+  """
+  return voroplusplus.compute_voronoi(points, limits, dispersion)
